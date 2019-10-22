ff6030388a5a8c7c25ab4fae0e69d179209e7fdb
vitaliy-developer <katcalo.v@gmail.com>
2019-10-22 18:11:47 +0300

    add 5.operator.py

---
+++ 5.operator.py
@@ -0,0 +1,28 @@
+# number = int(input("Enter a number: "))
+# if number > 5:
+#     print("This number > 5")
+# else:
+#     print("Number < 5")
+
+
+# name = input("Enter you name: ")
+
+# if name == "Alex":
+#     print("You have entered", name)
+#     print("This is my name, too.")
+# else:
+#     print("You not Alex")
+
+# x = int(input(" x = "))
+
+# if 0 < x < 7:
+#     print("значение")
+#     y = 2 * x - 5
+#     if y < 0:
+#         print("negative")
+#     else:
+#         if y > 0:
+#             print("positive")
+#         else: 
+#             print("y = 0")
+
