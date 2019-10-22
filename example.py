9c0c7aae1ebac897832ec25eae5537242db7bfa1
Vitaliy Katkalo <katcalo.v@gmail.com>
2019-10-22 08:51:06 +0300

    new file example, function print and input

---
+++ example.py
@@ -0,0 +1,21 @@
+# name = input('Как тебя зовут ?' '\n')
+# print('Hello,', name)
+
+# number = 50
+# print(number)
+
+# string = '10'
+# number = int(string)
+# print(number + 5)
+
+# a = 5.7
+# b = 2.2
+# print(a + b)
+
+# var = 44
+# print(var)
+# print(type(var))
+
+n = int(input('first number: '))
+m = int(input('Second number: '))
+print('{} + {} = {}'.format(n, m, n + m))
