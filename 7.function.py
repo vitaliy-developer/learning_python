1c99251705b5c7518031ad5445e54a9611ff95d6
vitaliy-developer <katcalo.v@gmail.com>
2019-10-30 12:05:56 +0200

    code python

--- 7.function.py
+++ 7.function.py
@@ -3,3 +3,19 @@ def print_numbers(limit):
         print(i)
 n = int(input("n = "))
 print_numbers(n)
+
+def hello():
+    print("Hello World!")
+    print("this text gets printed from a function")
+    print()
+hello()
+hello()
+
+def print_info(object_name, color, price):
+    print('Object:', object_name)
+    print('Color:', color)
+    print('Price:', price)
+print_info('pen', 'blue', 1)
+print_info(price=5, object_name='coffe', color='black')
+
+ssh remoteuser@remoteserver
